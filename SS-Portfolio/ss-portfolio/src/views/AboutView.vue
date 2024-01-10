bb69f922962cdee3bf688814fd3dd8e5042f65fb
shaheemux <120124594+shaheemux@users.noreply.github.com>
2024-01-10 11:06:00 +0200

    added files

--- SS-Portfolio/ss-portfolio/src/views/AboutView.vue
+++ SS-Portfolio/ss-portfolio/src/views/AboutView.vue
@@ -1,2 +1,35 @@
 <template>
+  <div class="about">
+    <p  class="description">{{$store.state.about.description}}</p>
+    <p>{{$store.state.about.hobbies}}</p>
+  </div>
 </template>
+<script>
+export default {
+    computed:{
+        fetchDataAbout(){
+            this.$store.dispatch('fetchDataAbout')
+        }
+    },
+    mounted(){
+        this.fetchDataAbout
+    }
+}
+</script>
+
+<style scoped>
+.about{
+  border: solid 1px solid;
+  background-color: aqua;
+  width: 50%;
+}
+.description{
+  padding: 1rem;
+}
+</style>
+
+
+
+
+
+
--- SS-Portfolio/ss-portfolio/src/views/HomeView.vue
+++ SS-Portfolio/ss-portfolio/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 !<template>
-  
+  <div class="Home">
+
+  </div>
 </template>
 
 <script>
