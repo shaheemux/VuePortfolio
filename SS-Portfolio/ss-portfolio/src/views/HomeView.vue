8fab7b634640d9ba159f0c27162e6a588d201f67
unknown <Shaheemsalie349@gmail.com>
2024-01-11 15:10:35 -0500

    added files

--- SS-Portfolio/ss-portfolio/src/views/HomeView.vue
+++ SS-Portfolio/ss-portfolio/src/views/HomeView.vue
@@ -36,5 +36,46 @@ export default {
     margin-top: 4.5rem;
 }
 
+@media (max-width: 1080px) {
+    .title {
+        font-size: 5rem; /* Adjust font size and other styles as needed */
+    }
+}
+
+@media (max-width: 720px) {
+    .title {
+        font-size: 4rem; /* Adjust font size and other styles as needed */
+    }
+}
+
+@media (max-width: 650px) {
+    .title {
+        font-size: 3.5rem; /* Adjust font size and other styles as needed */
+    }
+}
+
+@media (max-width: 480px) {
+    .title {
+        font-size: 3rem; /* Adjust font size and other styles as needed */
+        top: 50px; /* Adjust position as needed */
+    }
+}
+
+@media (max-width: 320px) {
+    .title {
+        font-size: 2.5rem; /* Adjust font size and other styles as needed */
+        top: 20px; /* Adjust position as needed */
+    }
+}
+
+@media (max-width: 280px) {
+    .title {
+        font-size: 2rem; /* Adjust font size and other styles as needed */
+        top: 10px; /* Adjust position as needed */
+        padding: 0.5rem; /* Adjust padding as needed */
+    }
+}
+
+
 
 </style>
