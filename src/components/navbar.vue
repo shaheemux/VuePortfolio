7d82093fb662d0cae085db3d1bb7e808fae62181
shaheemux <120124594+shaheemux@users.noreply.github.com>
2024-01-24 16:25:34 +0200

    added files

---
+++ src/components/navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="navbar">   
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active" aria-current="page" href="#">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="" class="nav-link">Pricing</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </div>
+  
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+   
+  .navbar{
+    background-color: #282c34 !important;
+    
+     
+  }
+  .nav-link{
+    color: white !important;
+  }
+  
+  </style>
+ 
