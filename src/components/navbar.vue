08f264b2f468e27eabbeb39374dafe720ce09c79
unknown <Shaheemsalie349@gmail.com>
2024-01-24 14:19:36 -0500

    added files

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,6 +2,7 @@
   <div id="navbar">   
     <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark">
     <div class="container-fluid">
+      <h3 style="color: #e4bb68;   font-family: JetBrains Mono, monospace;">Shaheem<span style="  color:#c678dd;"><span style="color: #c678dd;">.</span>Salie</span></h3>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -14,7 +15,7 @@
             <router-link to="/about" class="nav-link">About</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="" class="nav-link">Pricing</router-link>
+            <router-link to="" class="nav-link">Resume</router-link>
           </li>
         </ul>
       </div>
@@ -33,12 +34,13 @@
   <style scoped>
    
   .navbar{
-    background-color: #282c34 !important;
-    
-     
+    background-color: #393d46 !important;     
+  }
+  .navbarNav{   
+    text-align: center !important;
+
   }
   .nav-link{
-    
     color: white !important;
   }
   
